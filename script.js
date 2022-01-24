564d79f35f3a554aca9614e043f6e9a5b69d9374
yersel <yersel500@gmail.com>
2022-01-24 18:25:59 -0500

    Fixing style and linters comments

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const navMenu = document.querySelector('#main-menu');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-}))
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
