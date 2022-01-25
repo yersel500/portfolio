6e51c0f903e5188ae8fc150b61bf064643315f37
yersel <yersel500@gmail.com>
2022-01-25 16:22:03 -0500

    Add js object to card 2

--- script.js
+++ script.js
@@ -52,3 +52,38 @@ function Project(name, description, image, languages, live, source) {
   
   document.getElementById('source').innerHTML = project1.projectSource;
 
+  const project1m = new Project('Tonica', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent", './images/portfolio-desktop/portfolio-1.png', {lg1: 'test-html', lg2: 'test-css', lg3: 'test-javaScript'}, 'https://yersel500.github.io/portfolio/', 'https://github.com/yersel500/portfolio');
+  
+  document.getElementById('title1-m').innerHTML = project1m.projectName;
+  
+  document.getElementById('descrip1-m').innerHTML = project1m.projectDescription;
+  
+  document.getElementById('img1-m').src = project1m.projectImage;
+  
+  document.getElementById('lg1-m').innerHTML = project1m.projectLanguages.lg1;
+  
+  document.getElementById('lg2-m').innerHTML = project1m.projectLanguages.lg2;
+  
+  document.getElementById('lg3-m').innerHTML = project1m.projectLanguages.lg3;
+  
+  document.getElementById('live-m').innerHTML = project1m.Live;
+  
+  document.getElementById('source-m').innerHTML = project1m.projectSource;
+
+  const project2 = new Project('Tonica2', 'Test: A daily selection of privately personalized reads; no accounts or sign-ups required.', './images/portfolio-desktop/portfolio-1.png', {lg1: 'test-html', lg2: 'test-css', lg3: 'test-javaScript'}, 'https://yersel500.github.io/portfolio/', 'https://github.com/yersel500/portfolio');
+  
+  document.getElementById('title2').innerHTML = project1.projectName;
+  
+  document.getElementById('descrip2').innerHTML = project1.projectDescription;
+  
+  document.getElementById('img2').src = project1.projectImage;
+  
+  document.getElementById('lg1').innerHTML = project1.projectLanguages.lg1;
+  
+  document.getElementById('lg2').innerHTML = project1.projectLanguages.lg2;
+  
+  document.getElementById('lg3').innerHTML = project1.projectLanguages.lg3;
+  
+  document.getElementById('live').innerHTML = project1.Live;
+  
+  document.getElementById('source').innerHTML = project1.projectSource;
