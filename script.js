7221d45254b7f91767ab0da2b413b40cd255b1b0
yersel <yersel500@gmail.com>
2022-01-25 14:45:18 -0500

    Create js object for cards

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+function Project(name, description, image, languages, live, source) {
+
+  this.projectName=name;
+  
+  this.projectDescription=description;
+  
+  this.projectImage=image;
+  
+  this.projectLanguages=languages;
+  
+  this.projectLive=live;
+  
+  this.projectSource=source;
+  
+  }
+  
+  const project1 = new Project('Tonica', 'Test: A daily selection of privately personalized reads; no accounts or sign-ups required.', './images/portfolio-desktop/portfolio-1.png', {lg1: 'test-html', lg2: 'test-css', lg3: 'test-javaScript'}, 'https://yersel500.github.io/portfolio/', 'https://github.com/yersel500/portfolio');
+  
+  document.getElementById('title1').innerHTML = project1.projectName;
+  
+  document.getElementById('descrip1').innerHTML = project1.projectDescription;
+  
+  document.getElementById('img1').src = project1.projectImage;
+  
+  document.getElementById('lg1').innerHTML = project1.projectLanguages.lg1;
+  
+  document.getElementById('lg2').innerHTML = project1.projectLanguages.lg2;
+  
+  document.getElementById('lg3').innerHTML = project1.projectLanguages.lg3;
+  
+  document.getElementById('live').innerHTML = project1.Live;
+  
+  document.getElementById('source').innerHTML = project1.projectSource;
